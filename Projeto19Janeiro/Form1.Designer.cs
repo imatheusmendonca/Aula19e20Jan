697f9ebe39d352206e2d9ff7c13ced6f61d32da3
imatheusmendonca <120023913+imatheusmendonca@users.noreply.github.com>
2023-01-20 11:04:19 -0300

    Add files via upload

---
+++ Projeto19Janeiro/Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace Projeto19Janeiro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt7 = new System.Windows.Forms.Button();
+            this.bt3 = new System.Windows.Forms.Button();
+            this.bt2 = new System.Windows.Forms.Button();
+            this.bt1 = new System.Windows.Forms.Button();
+            this.bt6 = new System.Windows.Forms.Button();
+            this.bt5 = new System.Windows.Forms.Button();
+            this.bt4 = new System.Windows.Forms.Button();
+            this.bt9 = new System.Windows.Forms.Button();
+            this.bt8 = new System.Windows.Forms.Button();
+            this.bt0 = new System.Windows.Forms.Button();
+            this.btresu = new System.Windows.Forms.TextBox();
+            this.mais = new System.Windows.Forms.Button();
+            this.ce = new System.Windows.Forms.Button();
+            this.menos = new System.Windows.Forms.Button();
+            this.c = new System.Windows.Forms.Button();
+            this.vezes = new System.Windows.Forms.Button();
+            this.barra = new System.Windows.Forms.Button();
+            this.igual = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bt7
+            // 
+            this.bt7.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.bt7.Font = new System.Drawing.Font("Britannic Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt7.ForeColor = System.Drawing.SystemColors.Control;
+            this.bt7.Location = new System.Drawing.Point(16, 74);
+            this.bt7.Name = "bt7";
+            this.bt7.Size = new System.Drawing.Size(53, 59);
+            this.bt7.TabIndex = 0;
+            this.bt7.Text = "7";
+            this.bt7.UseVisualStyleBackColor = false;
+            this.bt7.Click += new System.EventHandler(this.bt7_Click);
+            // 
+            // bt3
+            // 
+            this.bt3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.bt3.Font = new System.Drawing.Font("Britannic Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt3.ForeColor = System.Drawing.SystemColors.Control;
+            this.bt3.Location = new System.Drawing.Point(134, 206);
+            this.bt3.Name = "bt3";
+            this.bt3.Size = new System.Drawing.Size(53, 59);
+            this.bt3.TabIndex = 1;
+            this.bt3.Text = "3";
+            this.bt3.UseVisualStyleBackColor = false;
+            this.bt3.Click += new System.EventHandler(this.bt3_Click);
+            // 
+            // bt2
+            // 
+            this.bt2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.bt2.Font = new System.Drawing.Font("Britannic Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt2.ForeColor = System.Drawing.SystemColors.Control;
+            this.bt2.Location = new System.Drawing.Point(75, 204);
+            this.bt2.Name = "bt2";
+            this.bt2.Size = new System.Drawing.Size(53, 59);
+            this.bt2.TabIndex = 2;
+            this.bt2.Text = "2";
+            this.bt2.UseVisualStyleBackColor = false;
+            this.bt2.Click += new System.EventHandler(this.bt2_Click);
+            // 
+            // bt1
+            // 
+            this.bt1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.bt1.Font = new System.Drawing.Font("Britannic Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt1.ForeColor = System.Drawing.SystemColors.Control;
+            this.bt1.Location = new System.Drawing.Point(16, 204);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(53, 59);
+            this.bt1.TabIndex = 3;
+            this.bt1.Text = "1";
+            this.bt1.UseVisualStyleBackColor = false;
+            this.bt1.Click += new System.EventHandler(this.bt1_Click);
+            // 
+            // bt6
+            // 
+            this.bt6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.bt6.Font = new System.Drawing.Font("Britannic Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt6.ForeColor = System.Drawing.SystemColors.Control;
+            this.bt6.Location = new System.Drawing.Point(134, 139);
+            this.bt6.Name = "bt6";
+            this.bt6.Size = new System.Drawing.Size(53, 59);
+            this.bt6.TabIndex = 4;
+            this.bt6.Text = "6";
+            this.bt6.UseVisualStyleBackColor = false;
+            this.bt6.Click += new System.EventHandler(this.bt6_Click);
+            // 
+            // bt5
+            // 
+            this.bt5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.bt5.Font = new System.Drawing.Font("Britannic Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt5.ForeColor = System.Drawing.SystemColors.Control;
+            this.bt5.Location = new System.Drawing.Point(75, 139);
+            this.bt5.Name = "bt5";
+            this.bt5.Size = new System.Drawing.Size(53, 59);
+            this.bt5.TabIndex = 5;
+            this.bt5.Text = "5";
+            this.bt5.UseVisualStyleBackColor = false;
+            this.bt5.Click += new System.EventHandler(this.bt5_Click);
+            // 
+            // bt4
+            // 
+            this.bt4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.bt4.Font = new System.Drawing.Font("Britannic Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt4.ForeColor = System.Drawing.SystemColors.Control;
+            this.bt4.Location = new System.Drawing.Point(16, 139);
+            this.bt4.Name = "bt4";
+            this.bt4.Size = new System.Drawing.Size(53, 59);
+            this.bt4.TabIndex = 6;
+            this.bt4.Text = "4";
+            this.bt4.UseVisualStyleBackColor = false;
+            this.bt4.Click += new System.EventHandler(this.bt4_Click);
+            // 
+            // bt9
+            // 
+            this.bt9.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.bt9.Font = new System.Drawing.Font("Britannic Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt9.ForeColor = System.Drawing.SystemColors.Control;
+            this.bt9.Location = new System.Drawing.Point(134, 74);
+            this.bt9.Name = "bt9";
+            this.bt9.Size = new System.Drawing.Size(53, 59);
+            this.bt9.TabIndex = 7;
+            this.bt9.Text = "9";
+            this.bt9.UseVisualStyleBackColor = false;
+            this.bt9.Click += new System.EventHandler(this.bt9_Click);
+            // 
+            // bt8
+            // 
+            this.bt8.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.bt8.Font = new System.Drawing.Font("Britannic Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt8.ForeColor = System.Drawing.SystemColors.Control;
+            this.bt8.Location = new System.Drawing.Point(75, 74);
+            this.bt8.Name = "bt8";
+            this.bt8.Size = new System.Drawing.Size(53, 59);
+            this.bt8.TabIndex = 8;
+            this.bt8.Text = "8";
+            this.bt8.UseVisualStyleBackColor = false;
+            this.bt8.Click += new System.EventHandler(this.bt8_Click);
+            // 
+            // bt0
+            // 
+            this.bt0.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.bt0.Font = new System.Drawing.Font("Britannic Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt0.ForeColor = System.Drawing.SystemColors.Control;
+            this.bt0.Location = new System.Drawing.Point(16, 271);
+            this.bt0.Name = "bt0";
+            this.bt0.Size = new System.Drawing.Size(176, 59);
+            this.bt0.TabIndex = 12;
+            this.bt0.Text = "0";
+            this.bt0.UseVisualStyleBackColor = false;
+            this.bt0.Click += new System.EventHandler(this.bt0_Click);
+            // 
+            // btresu
+            // 
+            this.btresu.BackColor = System.Drawing.SystemColors.InfoText;
+            this.btresu.Font = new System.Drawing.Font("Britannic Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btresu.ForeColor = System.Drawing.SystemColors.Menu;
+            this.btresu.Location = new System.Drawing.Point(16, 0);
+            this.btresu.Multiline = true;
+            this.btresu.Name = "btresu";
+            this.btresu.Size = new System.Drawing.Size(289, 68);
+            this.btresu.TabIndex = 13;
+            this.btresu.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.btresu.TextChanged += new System.EventHandler(this.tbresu_TextChanged);
+            // 
+            // mais
+            // 
+            this.mais.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.mais.Font = new System.Drawing.Font("Britannic Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mais.ForeColor = System.Drawing.SystemColors.Control;
+            this.mais.Location = new System.Drawing.Point(193, 74);
+            this.mais.Name = "mais";
+            this.mais.Size = new System.Drawing.Size(53, 59);
+            this.mais.TabIndex = 17;
+            this.mais.Text = "+";
+            this.mais.UseVisualStyleBackColor = false;
+            this.mais.Click += new System.EventHandler(this.mais_Click);
+            // 
+            // ce
+            // 
+            this.ce.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ce.Font = new System.Drawing.Font("Britannic Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ce.ForeColor = System.Drawing.SystemColors.Control;
+            this.ce.Location = new System.Drawing.Point(252, 74);
+            this.ce.Name = "ce";
+            this.ce.Size = new System.Drawing.Size(53, 59);
+            this.ce.TabIndex = 16;
+            this.ce.Text = "CE";
+            this.ce.UseVisualStyleBackColor = false;
+            this.ce.Click += new System.EventHandler(this.ce_Click);
+            // 
+            // menos
+            // 
+            this.menos.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.menos.Font = new System.Drawing.Font("Britannic Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menos.ForeColor = System.Drawing.SystemColors.Control;
+            this.menos.Location = new System.Drawing.Point(193, 139);
+            this.menos.Name = "menos";
+            this.menos.Size = new System.Drawing.Size(53, 59);
+            this.menos.TabIndex = 15;
+            this.menos.Text = "-";
+            this.menos.UseVisualStyleBackColor = false;
+            this.menos.Click += new System.EventHandler(this.menos_Click);
+            // 
+            // c
+            // 
+            this.c.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.c.Font = new System.Drawing.Font("Britannic Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c.ForeColor = System.Drawing.SystemColors.Control;
+            this.c.Location = new System.Drawing.Point(252, 141);
+            this.c.Name = "c";
+            this.c.Size = new System.Drawing.Size(53, 59);
+            this.c.TabIndex = 14;
+            this.c.Text = "C";
+            this.c.UseVisualStyleBackColor = false;
+            this.c.Click += new System.EventHandler(this.c_Click);
+            // 
+            // vezes
+            // 
+            this.vezes.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.vezes.Font = new System.Drawing.Font("Britannic Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vezes.ForeColor = System.Drawing.SystemColors.Control;
+            this.vezes.Location = new System.Drawing.Point(193, 206);
+            this.vezes.Name = "vezes";
+            this.vezes.Size = new System.Drawing.Size(53, 59);
+            this.vezes.TabIndex = 19;
+            this.vezes.Text = "X";
+            this.vezes.UseVisualStyleBackColor = false;
+            this.vezes.Click += new System.EventHandler(this.vezes_Click);
+            // 
+            // barra
+            // 
+            this.barra.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.barra.Font = new System.Drawing.Font("Britannic Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.barra.ForeColor = System.Drawing.SystemColors.Control;
+            this.barra.Location = new System.Drawing.Point(193, 269);
+            this.barra.Name = "barra";
+            this.barra.Size = new System.Drawing.Size(53, 59);
+            this.barra.TabIndex = 18;
+            this.barra.Text = "/";
+            this.barra.UseVisualStyleBackColor = false;
+            this.barra.Click += new System.EventHandler(this.barra_Click);
+            // 
+            // igual
+            // 
+            this.igual.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.igual.Font = new System.Drawing.Font("Britannic Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.igual.ForeColor = System.Drawing.SystemColors.Control;
+            this.igual.Location = new System.Drawing.Point(247, 206);
+            this.igual.Name = "igual";
+            this.igual.Size = new System.Drawing.Size(58, 122);
+            this.igual.TabIndex = 20;
+            this.igual.Text = "=";
+            this.igual.UseVisualStyleBackColor = false;
+            this.igual.Click += new System.EventHandler(this.igual_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("Britannic Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(23, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 21);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "OPERAÇÃO";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 346);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.igual);
+            this.Controls.Add(this.vezes);
+            this.Controls.Add(this.barra);
+            this.Controls.Add(this.mais);
+            this.Controls.Add(this.ce);
+            this.Controls.Add(this.menos);
+            this.Controls.Add(this.c);
+            this.Controls.Add(this.btresu);
+            this.Controls.Add(this.bt0);
+            this.Controls.Add(this.bt8);
+            this.Controls.Add(this.bt9);
+            this.Controls.Add(this.bt4);
+            this.Controls.Add(this.bt5);
+            this.Controls.Add(this.bt6);
+            this.Controls.Add(this.bt1);
+            this.Controls.Add(this.bt2);
+            this.Controls.Add(this.bt3);
+            this.Controls.Add(this.bt7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt7;
+        private System.Windows.Forms.Button bt3;
+        private System.Windows.Forms.Button bt2;
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.Button bt6;
+        private System.Windows.Forms.Button bt5;
+        private System.Windows.Forms.Button bt4;
+        private System.Windows.Forms.Button bt9;
+        private System.Windows.Forms.Button bt8;
+        private System.Windows.Forms.Button bt0;
+        private System.Windows.Forms.TextBox btresu;
+        private System.Windows.Forms.Button mais;
+        private System.Windows.Forms.Button ce;
+        private System.Windows.Forms.Button menos;
+        private System.Windows.Forms.Button c;
+        private System.Windows.Forms.Button vezes;
+        private System.Windows.Forms.Button barra;
+        private System.Windows.Forms.Button igual;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Projeto19Janeiro/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto19Janeiro
+{
+    public partial class Form1 : Form
+    {
+        decimal valor1 = 0;
+        decimal valor2 = 0;
+        string operacao;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ce_Click(object sender, EventArgs e)
+        {
+            valor1 = decimal.Parse(btresu.Text, CultureInfo.InvariantCulture);
+
+            btresu.Text = "";
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt7_Click(object sender, EventArgs e)
+        {
+            btresu.Text=btresu.Text+"7";
+            
+        }
+
+        private void tbresu_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void bt8_Click(object sender, EventArgs e)
+        {
+            btresu.Text = btresu.Text + "8";
+        }
+
+        private void bt9_Click(object sender, EventArgs e)
+        {
+            btresu.Text = btresu.Text + "9";
+        }
+
+        private void bt4_Click(object sender, EventArgs e)
+        {
+            btresu.Text = btresu.Text + "4";
+        }
+
+        private void bt5_Click(object sender, EventArgs e)
+        {
+            btresu.Text = btresu.Text + "5";
+        }
+
+        private void bt6_Click(object sender, EventArgs e)
+        {
+            btresu.Text = btresu.Text + "6";
+        }
+
+        private void bt1_Click(object sender, EventArgs e)
+        {
+            btresu.Text = btresu.Text + "1";
+        }
+
+        private void bt2_Click(object sender, EventArgs e)
+        {
+            btresu.Text = btresu.Text + "2";
+        }
+
+        private void bt3_Click(object sender, EventArgs e)
+        {
+            btresu.Text = btresu.Text + "3";
+        }
+
+        private void bt0_Click(object sender, EventArgs e)
+        {
+            btresu.Text = btresu.Text + "0";
+        }
+
+        private void mais_Click(object sender, EventArgs e)
+        {
+            valor1 = decimal.Parse(btresu.Text, CultureInfo.InvariantCulture);
+
+            btresu.Text = "";
+            operacao = "soma";
+            label1.Text = "+";
+        }
+
+        private void menos_Click(object sender, EventArgs e)
+        {
+            valor1 = decimal.Parse(btresu.Text, CultureInfo.InvariantCulture);
+
+            btresu.Text = "";
+            operacao = "subtracao";
+            label1.Text = "-";
+        }
+
+        private void vezes_Click(object sender, EventArgs e)
+        {
+            valor1 = decimal.Parse(btresu.Text, CultureInfo.InvariantCulture);
+
+            btresu.Text = "";
+            operacao = "multiplicacao";
+            label1.Text = "x";
+        }
+
+        private void barra_Click(object sender, EventArgs e)
+        {
+            valor1 = decimal.Parse(btresu.Text, CultureInfo.InvariantCulture);
+
+            btresu.Text = "";
+            operacao = "divisao";
+            label1.Text = "/";
+        }
+
+        private void c_Click(object sender, EventArgs e)
+        {
+            valor1 = decimal.Parse(btresu.Text, CultureInfo.InvariantCulture);
+
+            btresu.Text = "";
+            valor1 = 0;
+            valor2 = 0;
+             
+            
+        
+        }
+
+        private void igual_Click(object sender, EventArgs e)
+        {
+            valor2 = decimal.Parse(btresu.Text, CultureInfo.InvariantCulture);
+
+            if (operacao == "soma")
+            {
+                btresu.Text = Convert.ToString(valor1 + valor2);
+         
+            }
+            if (operacao == "subtracao")
+            {
+                btresu.Text = Convert.ToString(valor1 - valor2);
+
+            }
+            if (operacao == "multiplicacao")
+            {
+                btresu.Text = Convert.ToString(valor1 * valor2);
+
+            }
+            if (operacao == "divisao")
+            {
+                btresu.Text = Convert.ToString(valor1 / valor2);
+
+            }
+        }
+        
+    }
+}
+
